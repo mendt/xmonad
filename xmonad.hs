70289f3ea8fe5b29ab23a855251fe028f1bb5f14
mendt <111298823+mendt@users.noreply.github.com>
2023-01-15 16:54:33 +0100

    Add files via upload

---
+++ xmonad.hs
@@ -0,0 +1,97 @@
+import XMonad
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Loggers
+import XMonad.Util.Ungrab
+
+import XMonad.Hooks.EwmhDesktops
+
+import XMonad.Layout.Spacing
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks
+
+main :: IO ()
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
+
+myConfig = def
+    { modMask    = mod4Mask      -- Rebind Mod to the Super key
+    , terminal   = "alacritty"
+    , layoutHook = smartSpacingWithEdge 2 $ myLayout -- Use custom layouts
+    , manageHook = myManageHook  -- Match on certain windows
+    , logHook    = myLogHook
+    , workspaces = myWorkspaces
+    , borderWidth = 0
+    }
+  `additionalKeysP`
+    [ ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("M-C-s", unGrab *> spawn "scrot -s"        )
+    , ("M-f"  , spawn "firefox")
+    , ("M-p"  , spawn "rofi -show")
+    , ("M-e", spawn "emacsclient -c")
+    , ("M-S-m", sendMessage ToggleStruts)
+    , ("M-<Space>", sendMessage NextLayout >> sendMessage ToggleStruts)
+    , ("<XF86AudioMute>", spawn "amixer set Master toggle")
+    , ("<XF86AudioRaiseVolume>", spawn "amixer set Master 5%+")
+    , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%-")
+    , ("<XF86AudioMicMute>", spawn "amixer set Capture toggle")
+    , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
+    , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
+    ]
+
+myLogHook :: X ()
+myLogHook = fadeInactiveLogHook fadeAmount
+  where
+    fadeAmount = 0.95
+
+myWorkspaces = ["dev","www","sys","doc","vbox","chat","mus","vid","gfx"]
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , isDialog            --> doFloat
+    ]
+
+myLayout = tiled |||  Full
+  where
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    --  Windows should have *some* title, which should not not exceed a sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 10
+    
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+    
